4ef3f1d59a82bfb5b6246ee7cc2ec265b442e60b
Magda <magdalenastachon26@gmail.com>
2016-05-15 14:57:03 +0200

    first com

--- src/com/project/to/plik/NagranieDzwiekowe.java
+++ src/com/project/to/plik/NagranieDzwiekowe.java
@@ -4,4 +4,39 @@ package com.project.to.plik;
  * Created by Magda on 2016-05-14.
  */
 public class NagranieDzwiekowe implements Plik {
+
+    int rozmiar;
+    String nazwa;
+    String rozszerzenie;
+
+    public NagranieDzwiekowe(int rozmiar, String nazwa, String rozszerzenie){
+        this.rozmiar=rozmiar;
+        this.nazwa=nazwa;
+        this.rozszerzenie=rozszerzenie;
+    }
+
+
+    @Override
+    public int getRozmiar() {
+       return rozmiar;
+    }
+
+    @Override
+    public String getNazwa() {
+       return nazwa;
+    }
+
+    @Override
+    public void wczytaj() {
+
+    }
+
+    @Override
+    public String getRozszerzenie() {
+        return rozszerzenie;
+    }
+
+    public String display(){
+        return nazwa+ " "+rozmiar+" "+rozszerzenie;
+    }
 }
